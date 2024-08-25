c186b0f0bf276a31a44b07a226ae0fb878b634aa
04stefke <jovanovics399@yahoo.com>
2024-08-25 19:15:50 +0100

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
-import React from "react";
 import "./App.css";
-import { getUserInfo } from "./api/api";
 import Form from "./components/Form";
 function App() {
-	const handleSubmit = (event: React.FormEvent) => {
-		event.preventDefault();
-		const inputValue = event.target.elements[0].value;
-		getUserInfo(inputValue);
-	};
-
 	return (
-		<>
+		<div className=" flex w-[100dvh]">
 			<Form />
-			{/* <form onSubmit={handleSubmit}>
-				<input type="text" />
-				<button type="submit"></button>
-			</form> */}
-		</>
+		</div>
 	);
 }
 
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import { getUserInfo } from "../api/api";
 
 function Form() {
-  return (
-    <div>
-        ahahahah
-    </div>
-  )
+	const handleSubmit = (event: React.FormEvent) => {
+		event.preventDefault();
+		const inputValue = event.target.elements[0].value;
+		getUserInfo(inputValue);
+	};
+	return (
+		<div className="flex justify-center items-center w-full">
+			<form
+				onSubmit={handleSubmit}
+				className=" flex items-center justify-center"
+			>
+				<input type="text" />
+				<button type="submit"></button>
+			</form>
+		</div>
+	);
 }
 
-export default Form
+export default Form;
